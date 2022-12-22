1c55d0228c95106d300ce2ee32ffcf1bd16545ff
Timothy Small <smalls.online@icloud.com>
2022-12-22 15:23:22 -0500

    Add AccountFieldItem

---
+++ src/Lib/Models/AccountFieldItem.cs
@@ -0,0 +1,15 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Blazoot.Lib.Models;
+
+public class AccountFieldItem : IAccountFieldItem
+{
+    [JsonPropertyName("name")]
+    public string Name { get; set; }
+
+    [JsonPropertyName("value")]
+    public string Value { get; set; }
+
+    [JsonPropertyName("verified_at")]
+    public DateTimeOffset? VerifiedAt { get; set; }
+}
---
+++ src/Lib/Models/interfaces/IAccountFieldItem.cs
@@ -0,0 +1,8 @@
+﻿namespace Blazoot.Lib.Models;
+
+public interface IAccountFieldItem
+{
+    string Name { get; set; }
+    string Value { get; set; }
+    DateTimeOffset? VerifiedAt { get; set; }
+}
