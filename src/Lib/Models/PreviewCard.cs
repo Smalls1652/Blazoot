7019acfb876a65589006bf6ccdd6baea8b2ae67c
Timothy Small <smalls.online@icloud.com>
2022-12-22 15:23:54 -0500

    Add PreviewCard

---
+++ src/Lib/Models/PreviewCard.cs
@@ -0,0 +1,48 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Blazoot.Lib.Models;
+
+public class PreviewCard : IPreviewCard
+{
+    [JsonPropertyName("url")]
+    public string? Url { get; set; }
+
+    [JsonPropertyName("title")]
+    public string? Title { get; set; }
+
+    [JsonPropertyName("description")]
+    public string? Description { get; set; }
+
+    [JsonPropertyName("type")]
+    public string? Type { get; set; }
+
+    [JsonPropertyName("author_name")]
+    public string? AuthorName { get; set; }
+
+    [JsonPropertyName("author_url")]
+    public string? AuthorUrl { get; set; }
+
+    [JsonPropertyName("provider_name")]
+    public string? ProviderName { get; set; }
+
+    [JsonPropertyName("provider_url")]
+    public string? ProviderUrl { get; set; }
+
+    [JsonPropertyName("html")]
+    public string? Html { get; set; }
+
+    [JsonPropertyName("width")]
+    public int Width { get; set; }
+
+    [JsonPropertyName("height")]
+    public int Height { get; set; }
+
+    [JsonPropertyName("image")]
+    public string? Image { get; set; }
+
+    [JsonPropertyName("embed_url")]
+    public string? EmbedUrl { get; set; }
+
+    [JsonPropertyName("blurhash")]
+    public string? BlurHash { get; set; }
+}
---
+++ src/Lib/Models/interfaces/IPreviewCard.cs
@@ -0,0 +1,19 @@
+﻿namespace Blazoot.Lib.Models;
+
+public interface IPreviewCard
+{
+    string? Url { get; set; }
+    string? Title { get; set; }
+    string? Description { get; set; }
+    string? Type { get; set; }
+    string? AuthorName { get; set; }
+    string? AuthorUrl { get; set; }
+    string? ProviderName { get; set; }
+    string? ProviderUrl { get; set; }
+    string? Html { get; set; }
+    int Width { get; set; }
+    int Height { get; set; }
+    string? Image { get; set; }
+    string? EmbedUrl { get; set; }
+    string? BlurHash { get; set; }
+}
