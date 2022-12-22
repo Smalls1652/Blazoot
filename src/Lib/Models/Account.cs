b639f73f0f80b6c07a5993910f97e0607a44fa0d
Timothy Small <smalls.online@icloud.com>
2022-12-22 15:23:35 -0500

    Add Account

---
+++ src/Lib/Models/Account.cs
@@ -0,0 +1,81 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Blazoot.Lib.Models;
+
+public class Account : IAccount
+{
+    [JsonPropertyName("id")]
+    public string Id { get; set; }
+
+    [JsonPropertyName("username")]
+    public string UserName { get; set; }
+
+    [JsonPropertyName("acct")]
+    public string AccountUri { get; set; }
+
+    [JsonPropertyName("display_name")]
+    public string DisplayName { get; set; }
+
+    [JsonPropertyName("locked")]
+    public bool Locked { get; set; }
+
+    [JsonPropertyName("bot")]
+    public bool Bot { get; set; }
+
+    [JsonPropertyName("group")]
+    public bool Group { get; set; }
+
+    [JsonPropertyName("discoverable")]
+    public bool Discoverable { get; set; }
+
+    [JsonPropertyName("noindex")]
+    public bool NoIndex { get; set; }
+
+    [JsonPropertyName("moved")]
+    public bool Moved { get; set; }
+
+    [JsonPropertyName("suspended")]
+    public bool Suspended { get; set; }
+
+    [JsonPropertyName("limited")]
+    public bool Limited { get; set; }
+
+    [JsonPropertyName("created_at")]
+    public DateTimeOffset CreatedAt { get; set; }
+
+    [JsonPropertyName("note")]
+    public string Note { get; set; }
+
+    [JsonPropertyName("url")]
+    public string URL { get; set; }
+
+    [JsonPropertyName("avatar")]
+    public string Avatar { get; set; }
+
+    [JsonPropertyName("avatar_static")]
+    public string AvatarStatic { get; set; }
+
+    [JsonPropertyName("header")]
+    public string Header { get; set; }
+
+    [JsonPropertyName("header_static")]
+    public string HeaderStatic { get; set; }
+
+    [JsonPropertyName("followers_count")]
+    public int FollowersCount { get; set; }
+
+    [JsonPropertyName("following_count")]
+    public int FollowingCount { get; set; }
+
+    [JsonPropertyName("statuses_count")]
+    public int StatusesCount { get; set; }
+
+    [JsonPropertyName("last_status_at")]
+    public DateTimeOffset? LastStatusAt { get; set; }
+
+    [JsonPropertyName("emojis")]
+    public IEnumerable<CustomEmoji> Emojis { get; set; }
+
+    [JsonPropertyName("fields")]
+    public IEnumerable<AccountFieldItem> Fields { get; set; }
+}
---
+++ src/Lib/Models/interfaces/IAccount.cs
@@ -0,0 +1,30 @@
+﻿namespace Blazoot.Lib.Models;
+
+public interface IAccount
+{
+     string Id { get; set; }
+     string UserName { get; set; }
+     string AccountUri { get; set; }
+     string DisplayName { get; set; }
+     bool Locked { get; set; }
+     bool Bot { get; set; }
+     bool Group { get; set; }
+     bool Discoverable { get; set; }
+     bool NoIndex { get; set; }
+     bool Moved { get; set; }
+     bool Suspended { get; set; }
+     bool Limited { get; set; }
+     DateTimeOffset CreatedAt { get; set; }
+     string Note { get; set; }
+     string URL { get; set; }
+     string Avatar { get; set; }
+     string AvatarStatic { get; set; }
+     string Header { get; set; }
+     string HeaderStatic { get; set; }
+     int FollowersCount { get; set; }
+     int FollowingCount { get; set; }
+     int StatusesCount { get; set; }
+     DateTimeOffset? LastStatusAt { get; set; }
+     IEnumerable<CustomEmoji> Emojis { get; set; }
+     IEnumerable<AccountFieldItem> Fields { get; set; }
+}
