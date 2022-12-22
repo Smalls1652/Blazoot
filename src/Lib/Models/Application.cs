cd765d0a2bdea8d755c8641efcce5103b9a042a0
Timothy Small <smalls.online@icloud.com>
2022-12-22 15:23:45 -0500

    Add Application

---
+++ src/Lib/Models/Application.cs
@@ -0,0 +1,24 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Blazoot.Lib.Models;
+
+public class Application : IApplication
+{
+    public Application()
+    {}
+
+    [JsonPropertyName("name")]
+    public string Name { get; set; } = null!;
+
+    [JsonPropertyName("website")]
+    public string? Website { get; set; }
+
+    [JsonPropertyName("vapid_key")]
+    public string VapidKey { get; set; } = null!;
+
+    [JsonPropertyName("client_id")]
+    public string? ClientId { get; set; }
+
+    [JsonPropertyName("client_secret")]
+    public string? ClientSecret { get; set; }
+}
---
+++ src/Lib/Models/interfaces/IApplication.cs
@@ -0,0 +1,10 @@
+﻿namespace Blazoot.Lib.Models;
+
+public interface IApplication
+{
+    string Name { get; set; }
+    string? Website { get; set; }
+    string VapidKey { get; set; }
+    string? ClientId { get; set; }
+    string? ClientSecret { get; set; }
+}
