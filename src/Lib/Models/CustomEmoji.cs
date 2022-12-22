925e109f72ae446a356514ced721277ba2e70a26
Timothy Small <smalls.online@icloud.com>
2022-12-22 15:23:03 -0500

    Add CustomEmoji

---
+++ src/Lib/Models/CustomEmoji.cs
@@ -0,0 +1,21 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Blazoot.Lib.Models;
+
+public class CustomEmoji : ICustomEmoji
+{
+    [JsonPropertyName("shortcode")]
+    public string Shortcode { get; set; }
+
+    [JsonPropertyName("url")]
+    public string Url { get; set; }
+
+    [JsonPropertyName("static_url")]
+    public string StaticUrl { get; set; }
+
+    [JsonPropertyName("visible_in_picker")]
+    public bool VisibleInPicker { get; set; }
+
+    [JsonPropertyName("category")]
+    public string Category { get; set; }
+}
---
+++ src/Lib/Models/interfaces/ICustomEmoji.cs
@@ -0,0 +1,10 @@
+﻿namespace Blazoot.Lib.Models;
+
+public interface ICustomEmoji
+{
+    string Shortcode { get; set; }
+    string Url { get; set; }
+    string StaticUrl { get; set; }
+    bool VisibleInPicker { get; set; }
+    string Category { get; set; }
+}
